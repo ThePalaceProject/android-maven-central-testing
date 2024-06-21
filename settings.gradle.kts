580c701f801621e16ca81e03fa1da0d7ca5f2123
Mark Raynsford <code@io7m.com>
2024-06-21 11:48:34 +0000

    Initial files.

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        google()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        google()
+    }
+}
+
+rootProject.name = "org.thepalaceproject.deploytest"
+
